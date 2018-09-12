57af4291e190cf5ca6bd8c7cf478875c1b0b13b2
truong21 <543@holbertonschool.com>
2018-09-12 18:51:52 +0000

    Add README.md and Tasks 0 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/* This program prints "Programming is like building a multilingual puzzle */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Intro to Holberton School Low Level Programming Tasks
+
+```
+The following projects are designed to help beginners learn C language.

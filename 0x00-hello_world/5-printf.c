d3f869fd3db20fbfe29824c05f4f9261388d46a5
truong21 <543@holbertonschool.com>
2018-09-12 19:01:05 +0000

    Add Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* print with proper grammar, but the outcome is a piece of art,*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

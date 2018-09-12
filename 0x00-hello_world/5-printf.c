a8a5833748ac341794d67c5249bda4ceebe7badd
truong21 <543@holbertonschool.com>
2018-09-12 19:39:57 +0000

    edited 4-puts.c and 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* print with proper grammar, but the outcome is a piece of art,*/
+/**
+ * main - Print "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* This program prints the size of various types on the
+ * computer it is compiled and run on.
+ */
+
+int main(void)
+{
+	

723854e1cccd9952fbc922f58da31bc605f58011
truong21 <543@holbertonschool.com>
2018-09-13 21:51:28 +0000

    add task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 48;
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+
+	a = 97;
+	while (a < 103)
+	{
+		putchar(a);
+		a++;
+	}
+	printf("\n");
+
+	return (0);
+}

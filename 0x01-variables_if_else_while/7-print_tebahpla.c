cfd472372a7770dfe895dbe0410924835f3487da
truong21 <543@holbertonschool.com>
2018-09-13 21:41:02 +0000

    add task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z' ; a >= 'a' ; a--)
+	{
+		putchar(a);
+	}
+	printf("\n");
+
+	return (0);
+}

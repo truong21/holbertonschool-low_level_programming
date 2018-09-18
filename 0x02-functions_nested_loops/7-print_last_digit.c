59bbbc0fed19c4b8ded409c35f39daf8f15995fc
truong21 <543@holbertonschool.com>
2018-09-18 23:09:51 +0000

    fixed task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,14 @@
  */
 int print_last_digit(int n)
 {
+	n = n % 10;
 	if (n < 0)
 	{
-		n = -n % 10;
+		n *= -1;
 		_putchar(n + '0');
 	}
 	else
-	{
-		n = n % 10;
 		_putchar(n + '0');
-	}
 
 	return (n);
 }

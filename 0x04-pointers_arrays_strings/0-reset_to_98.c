f8518c046f0490c768e8d3193dfcc40a81ac32ee
truong21 <543@holbertonschool.com>
2018-09-25 19:17:30 +0000

    fixes: betty compliant

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and upates the value
  * it points to to 98
+ * @n: value of variable to be reset
  * Return: Nothing
  */
 void reset_to_98(int *n)

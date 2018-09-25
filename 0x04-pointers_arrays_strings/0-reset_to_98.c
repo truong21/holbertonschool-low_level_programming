d1170b62a6f07f41f10c94aa2cccf0e300209529
truong21 <543@holbertonschool.com>
2018-09-25 19:15:16 +0000

    function that resets variable to 98

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and upates the value
+ * it points to to 98
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

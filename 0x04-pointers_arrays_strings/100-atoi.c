f6e9ec48fee38f107af4d73acf2417d404eea540
truong21 <543@holbertonschool.com>
2018-09-26 19:52:11 +0000

    hot fix

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,7 @@ int _atoi(char *s)
 	}
 	for (; s[i] != '\0' && s[i] >= '0' && s[i] <= '9'; i++)
 	{
-			num = num * 10 + (s[i] - '0');
+		num = num * 10 + (s[i] - '0');
 	}
 	num *= sign;
 	return (num);

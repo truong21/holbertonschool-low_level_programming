91a3f52ab6671d4e3f0aa4a9471c52ecd339d665
truong21 <543@holbertonschool.com>
2018-09-26 03:02:47 +0000

    function that copies string from src to dest

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int count;
 
-	for (count = 0; src[count] != '\0'; count++)
-	{
+	for (count = 0; src[count] != 0; count++)
 		dest[count] = src[count];
-	}
 	dest[count] = '\0';
 
 	return (dest);

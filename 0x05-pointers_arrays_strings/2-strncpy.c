6918ac7c42ca9429b1d032c6edb4a963907d20b6
truong21 <543@holbertonschool.com>
2018-09-27 21:48:05 +0000

    add while loop to add null at end of dest

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	for (j = 0; j < n && *(dest + j) != '\0'; j++)
+	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
 		*(dest + i) = *(src + j);
 		i++;
 	}
-
+	while (j != n)
+		dest[j++] = '\0';
 	return (dest);
 }

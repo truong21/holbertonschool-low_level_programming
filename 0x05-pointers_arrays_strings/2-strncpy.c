3304192eb9046e7c57da8931881a539f5d63088c
truong21 <543@holbertonschool.com>
2018-09-27 21:51:52 +0000

    change variable j to i in while loop

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -17,7 +17,7 @@ char *_strncpy(char *dest, char *src, int n)
 		*(dest + i) = *(src + j);
 		i++;
 	}
-	while (j != n)
-		dest[j++] = '\0';
+	while (i != n)
+		dest[i++] = '\0';
 	return (dest);
 }

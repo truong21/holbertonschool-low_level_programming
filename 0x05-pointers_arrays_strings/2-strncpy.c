ddb786ef7c2691464d56c3dbd817e4fbca03e660
truong21 <543@holbertonschool.com>
2018-09-27 20:17:05 +0000

    copies a string of n size from src to dest

---
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: string to be copied to from source string
+ * @src: string to be copied to the string at destination
+ * @n: number of bytes
+ * Return: pointer to string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	for (j = 0; j < n && *(dest + j) != '\0'; j++)
+	{
+		*(dest + i) = *(src + j);
+		i++;
+	}
+
+	return (dest);
+}

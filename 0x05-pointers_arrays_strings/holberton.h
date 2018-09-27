c51a70131d5957724acb8e95f858a6e4872627ec
truong21 <543@holbertonschool.com>
2018-09-27 18:33:44 +0000

    First commit of project 0x05 with holberton.h and _putchar.c

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,13 @@
+/* Declaration for all functions in 0x05. C - More Pointers, arrays,
+ * and string project
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);

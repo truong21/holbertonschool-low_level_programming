8506547603ccb3c4d5e341bc90f8784988378e00
truong21 <543@holbertonschool.com>
2018-09-27 18:55:08 +0000

    append string at src to string at dest

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to be appended by source string
+ * @src: string to be appended to the string at destination
+ * Return: pointer to string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; *(dest + i) < '\0'; i++)
+	{
+	}
+	for (j = 0; *(src + j) <= '\0'; j++)
+		*(dest + i) = *(src + j);
+
+	return (dest);
+}

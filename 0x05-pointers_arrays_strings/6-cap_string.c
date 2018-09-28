90195375c9ac09f56660dc7de6cd5d210f31a61b
truong21 <543@holbertonschool.com>
2018-09-28 02:14:36 +0000

    capitilzes all words of a string

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitilizes all words of a string
+ * @s: pointer to a string
+ * Return: string in uppercase
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i - 1] < 49 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+
+	return (s);
+}

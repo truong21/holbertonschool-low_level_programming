03dc91e2ddff3cf026c1323e7e0add17321fcbcc
truong21 <543@holbertonschool.com>
2018-10-01 22:00:59 +0000

    locate character in string

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#define NULL 0
+/**
+ * *_strchr - locates a character in a string
+ * @s: the string for locating a character
+ * @c: character to be found
+ * Return: a pointer to first occurence of charater c in string C; otherwise
+ * Null if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}

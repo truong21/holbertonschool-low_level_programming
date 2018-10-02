103a745bf88e0f4df588543bb3b724024a41f6bf
truong21 <543@holbertonschool.com>
2018-10-02 06:36:00 +0000

    use two copies of *a for summation of diagonals integer

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -9,23 +9,23 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1, sum2, *copy;
+	int i, j, sum1, sum2, *copy1, *copy2;
 
-	sum1 = 0;
-	copy = a;
+	sum1 = sum2 = 0;
+	copy1 = copy2 = a;
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				sum1 += *a;
-			a++;
+				sum1 += *copy1;
+			copy1++;
 		}
 		for (j = 0; j < size; j++)
 		{
 			if (j == size - 1 - i)
-				sum2 += *copy;
-			copy++;
+				sum2 += *copy2;
+			copy2++;
 		}
 	}
 	printf("%d, %d\n", sum1, sum2);

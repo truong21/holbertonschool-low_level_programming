8220462192988d4f1f37064b01f6f85795cf351c
truong21 <543@holbertonschool.com>
2018-10-10 01:19:57 +0000

    remove use of strlen and strcpy

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  * _strdup - returns a pointer to a newly allocated space in memory,
  * which contains a copy of the string given as a parameter
@@ -10,13 +9,17 @@
 char *_strdup(char *str)
 {
 	char *dup_str;
+	int i, j;
 
-	dup_str = malloc(strlen(str) + 1);
-
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	dup_str = malloc((sizeof(char) * i) + 1);
 	if (dup_str == NULL)
 		return (NULL);
-	
-	strcpy(dup_str, str);
+	for (j = 0; j <= i; j++)
+		dup_str[j] = str[j];
 
 	return (dup_str);
 }

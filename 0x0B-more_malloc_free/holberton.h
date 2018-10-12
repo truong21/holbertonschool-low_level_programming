d4b92f0fe9aa1caf0549bfb50ff121e5ea8708ca
truong21 <543@holbertonschool.com>
2018-10-12 01:57:05 +0000

    First commit of project 0x0B-more malloc, free and task 0

---
+++ 0x0B-more_malloc_free/holberton.h
@@ -0,0 +1,8 @@
+/* Declaration for all functions in 0x06. C - Pointers, arrays,
+ * and string project
+ */
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);

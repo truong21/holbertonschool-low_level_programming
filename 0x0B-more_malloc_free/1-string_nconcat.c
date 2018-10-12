d5497694feade496bb616e59c3805df993691322
truong21 <543@holbertonschool.com>
2018-10-12 03:53:52 +0000

    fixed malloc for n byte of s2

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s2[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0' && j < n; j++)
 		;
-	str = malloc((sizeof(char) * i) + (sizeof(char) * j) + 1);
+	str = malloc(sizeof(char) *(i + j + 1));
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)

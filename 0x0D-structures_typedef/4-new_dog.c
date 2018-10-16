3d6ee7445f9299f0936cb0a276434937129d3e7c
truong21 <543@holbertonschool.com>
2018-10-16 00:49:29 +0000

    change pointer name

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -10,13 +10,13 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *doggy;
+	struct dog *new_dog;
 
-	doggy = malloc(sizeof(struct dog));
-	if (doggy == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 		return (NULL);
-	doggy->name = name;
-	doggy->age = age;
-	doggy->owner = owner;
-	return (doggy);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
 }

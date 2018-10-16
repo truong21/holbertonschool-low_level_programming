47bf2af326faf83c8f809942e0e117d42c58b67f
truong21 <543@holbertonschool.com>
2018-10-16 01:51:46 +0000

    store name and owner with _strdup function

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -11,12 +11,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
+	char *temp_name, *temp_owner;
 
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	temp_name = _strdup(name);
+	if (temp_name == NULL)
+		return (NULL);
+	temp_owner = _strdup(owner);
+	if (temp_owner == NULL)
+		return (NULL);
+	new_dog->name = temp_name;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = temp_owner;
 	return (new_dog);
+}
+/**
+ * _strdup - returns a pointer to a copy string
+ * @str: string
+ * Return: pointer to the stored string; NUll otherwise
+ */
+char *_strdup(char *str)
+{
+	char *dup_str;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	dup_str = malloc((sizeof(char) * i) + 1);
+	if (dup_str == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		dup_str[j] = str[j];
+
+	return (dup_str);
 }

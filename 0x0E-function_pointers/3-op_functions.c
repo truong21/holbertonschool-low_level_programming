a13a49e68b956720b6c543dcaf5971e19d458094
truong21 <543@holbertonschool.com>
2018-10-18 06:23:14 +0000

    operation functions

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * op_add - sum of a and b
+ * @a: integer a
+ * @b: integer b
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction of a by b
+ * @a: integer a
+ * @b: integer b
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply a with b
+ * @a: integer a
+ * @b: integer b
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide a by b
+ * @a: integer a
+ * @b: integer b
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - calculate remainder
+ * @a: integer a
+ * @b: integer b
+ * Return: remainder of the division of a by b
+ */
+int op_mul(int a, int b)
+{
+	return (a % b);
+}

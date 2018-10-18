996f9ac7f0f213b807ad08e507053a67597e6622
truong21 <543@holbertonschool.com>
2018-10-18 21:43:41 +0000

    initilize i = 0

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -12,10 +12,11 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div}, {"%", op_mod}, {NULL, NULL}};
 	int i;
 
-	while (i < 6)
+	i = 0;
+	while (i < 5)
 	{
-		if (*s == *(ops[i]).op)
-			return (*(ops[i]).f);
+		if (*s == *(ops[i].op))
+			return (ops[i].f);
 		i++;
 	}
 	return (NULL);

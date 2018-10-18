7c7156940d81a31fdc75c8699576a713b02d9a89
truong21 <543@holbertonschool.com>
2018-10-18 21:21:33 +0000

    update task 3 files

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -8,13 +8,13 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t operate[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul},
+	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul},
 		{"/", op_div}, {"%", op_mod}, {NULL, NULL}};
 	int i;
 
-	while (i < 5)
+	while (i < 6)
 	{
-		if (*s == *(operate[i]).op)
+		if (*s == *(ops[i]).op)
 			return (*(ops[i]).f);
 		i++;
 	}
---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "3-calc.h"
+/**
+ * main - performs arithmetic operations
+ * @argc - argument count
+ * @argv - integers and operater
+ * Return: 0 (Success); 98, 99, or 100 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	char op;
+	int num1, num2, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op = argv[2][0];
+	if ((op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
+		|| argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((op == '/' || op == '%') && num2  == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = get_op_func(argv[2])(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}

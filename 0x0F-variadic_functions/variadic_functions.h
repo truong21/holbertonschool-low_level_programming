868e0d8dabc917fdad44562fbc18ba829adea6b5
truong21 <543@holbertonschool.com>
2018-10-22 03:44:33 +0000

    betty style fixes

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -4,5 +4,18 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct types - Struct for data types and function pointers
+ * @type: The data type to be specified by print_all
+ * @f: The corresponding function pointer for each data type
+ *
+ * Description: Data types specified by print_all have corresponding functions
+ */
+typedef struct types
+{
+	char *type;
+	void (*f)();
+} types_t;
 void print_all(const char * const format, ...);
 #endif

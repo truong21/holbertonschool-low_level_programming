89fbaca76f35df214fed4d23bff480df9b963bef
truong21 <543@holbertonschool.com>
2018-10-26 07:16:16 +0000

    betty style fixes

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -12,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);

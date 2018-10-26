e6f73b5374a04831a25d16701555f316c0432c21
truong21 <543@holbertonschool.com>
2018-10-26 01:53:54 +0000

    first commit of 0x011-singly_linked_list of project and task 0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to list_t list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+	char *s;
+	unsigned int length;
+
+	n = 0;
+	while (h != NULL)
+	{
+		s = h->str;
+		length = h->len;
+		if (!s)
+		{
+			s = "(nil)";
+			length = 0;
+		}
+		printf("[%d] %s\n", length, s);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}	

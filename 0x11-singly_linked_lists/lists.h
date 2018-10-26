030c7299c6c5546ce97d13be2c8523a3d5782f49
truong21 <543@holbertonschool.com>
2018-10-26 03:28:30 +0000

    updated with more function prototypes

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -17,4 +17,9 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+void free_list(list_t *head);
+int _strlen(const char *s);
+list_t *add_node(list_t **head, const char *str);
 #endif

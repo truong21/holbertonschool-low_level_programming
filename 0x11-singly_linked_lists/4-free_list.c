36b91d80ef4f02e771bd2ac16eeec23b0b068984
truong21 <543@holbertonschool.com>
2018-10-26 06:14:51 +0000

    free list_t list

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_list - frees a list_t list
+ * @head: head of list
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}

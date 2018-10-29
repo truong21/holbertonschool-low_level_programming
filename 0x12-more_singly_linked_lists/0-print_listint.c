94886b8347193e48f6f717872f5be41af46291bb
truong21 <543@holbertonschool.com>
2018-10-29 16:03:15 +0000

    first commit of 0x12-more singly linked lists project and task 0 - prints elements of a lintint_t list

---
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - prints all the elements of a list_t list
+ * @h: pointer to head of list_t list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}

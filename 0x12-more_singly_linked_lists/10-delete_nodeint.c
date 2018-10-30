4f43091cb77813f5ed4fa52583d641a40d768fb3
truong21 <543@holbertonschool.com>
2018-10-30 02:16:36 +0000

    delete nth node at index index of a listint_t linked list

---
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * delete_nodeint_at_index - deletes the node at index, index, of a listint_t
+ * linked list
+ * @head: double pointer to head node
+ * @index: index of the list where the node should be deleted
+ * @n: integer to be added to new node
+ * Return: 1 (Success); -1 (Fail)
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *tmp2;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+	tmp = tmp2 = *head;
+	tmp2 = tmp2->next;
+	if (index == 0 && *head != NULL)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+	for (i = 0; i < (index - 1); i++)
+	{
+		tmp = tmp->next;
+		tmp2 = tmp2->next;
+	}
+	tmp->next = tmp2->next;
+	free(tmp2);
+	return (1);
+}

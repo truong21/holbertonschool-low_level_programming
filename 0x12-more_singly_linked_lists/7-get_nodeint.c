9493dd906e1226eb256ed58ca6ed10d6a5870d38
truong21 <543@holbertonschool.com>
2018-10-30 23:01:22 +0000

    check if head is null after loop

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -3,15 +3,16 @@
 /**
  * get_nodeint_at_index - returns the nth node of a listint_t linked list
  * @head: double pointer to the head of the listint_t list
+ * @index: index of list
  * Return: pointer to the nth node of list
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (!head)
-		return (NULL);
-	for (i = 0; i < index; i++)
+	for (i = 0; i < index && head != NULL; i++)
 		head = head->next;
+	if (head == NULL)
+		return (NULL);
 	return (head);
 }

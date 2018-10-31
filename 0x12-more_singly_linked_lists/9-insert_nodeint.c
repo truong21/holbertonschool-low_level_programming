66c22da80923aa8697263ad298b959df65676bbc
truong21 <543@holbertonschool.com>
2018-10-31 00:10:42 +0000

    add null edge case when index is beyond list

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
-	if (*head == NULL && n)
+	if (*head == NULL && idx > 0)
 	{
 		free(new_node);
 		return (NULL);
@@ -31,7 +31,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	else
 	{
 		for (i = 0; i < (idx - 1); i++)
+		{
 			tmp = tmp->next;
+			if (tmp == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+		}
 		new_node->next = tmp->next;
 		tmp->next = new_node;
 	}

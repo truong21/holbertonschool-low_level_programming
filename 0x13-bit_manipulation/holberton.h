a95675d427647dcacfb97d99d3c7ce2737c41369
truong21 <543@holbertonschool.com>
2018-11-01 19:41:09 +0000

    remove power function

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -1,6 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-unsigned int _pow(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);

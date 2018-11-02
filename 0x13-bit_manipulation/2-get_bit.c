4f743708618650837b27da92691cb0c521333c6f
truong21 <543@holbertonschool.com>
2018-11-02 02:53:19 +0000

    testing index > sizeof(unsigned long int) * 8

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
 
-	if (index > sizeof(unsigned long int) * 8 - 1)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 	n = n >> index;
 	if (n & 1)

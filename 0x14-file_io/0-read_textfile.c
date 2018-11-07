cc6131afc8ba7e492d7926b6ab25b4b6e76e2d88
truong21 <543@holbertonschool.com>
2018-11-07 05:02:50 +0000

    add case for characters read < than letters

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -20,21 +20,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
 	}
 	rd = read(fd, buffer, letters);
+	if (rd < 0)
+		return (0);
+	if ((size_t)rd < letters)
+		letters = rd;
 	wrt = write(1, buffer, letters);
 	if (wrt == -1)
 	{
 		free(buffer);
 		return (0);
 	}
-	if (rd != wrt)
-		return (0);
 	free(buffer);
 	cl = close(fd);
 	if (cl == -1)

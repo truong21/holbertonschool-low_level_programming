1c75d7fa31524a82e80c4981ce3c72a9e996110a
truong21 <543@holbertonschool.com>
2019-01-15 07:36:10 +0000

    fixed guard def

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -1,5 +1,6 @@
 #ifndef HASH_TABLES_H
-#define HAST_TABLES_H
+#define HASH_TABLES_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -28,8 +29,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);

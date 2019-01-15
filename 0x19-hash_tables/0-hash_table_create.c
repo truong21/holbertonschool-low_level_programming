3b31932035b242191adde516979e107e7b1c5286
truong21 <543@holbertonschool.com>
2019-01-15 03:43:33 +0000

    creates a hash table

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = malloc(sizeof(hash_table_t));
 	if (!ht)
 		return (NULL);
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (!(ht->array))
 		return (NULL);
 	for (i = 0; i < size; i++)

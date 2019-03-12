d3cfb9eac9641a6d4e6260f2cff36f5e0c650815
truong21 <543@holbertonschool.com>
2019-03-12 20:11:56 +0000

    first commit with README.md and task 0 - create a binary tree node

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a parent node in a binary tree
+ * @parent: parent node of the node to be created
+ * @value: value of new node
+ * Return: pointer to newly created node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	new_node->n = value;
+	new_node->left = new_node->right = NULL;
+	if (!new_node)
+		return (NULL);
+	if (!parent)
+		new_node->parent = NULL;
+	else
+		new_node->parent = parent;
+	
+	return (new_node);
+}	

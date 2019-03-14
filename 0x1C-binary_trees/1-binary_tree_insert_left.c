433683b8ddc7cbea38ffa4f3927bd6acf2371ff1
truong21 <543@holbertonschool.com>
2019-03-14 19:31:52 +0000

    use binary_tree_node in code

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -10,24 +10,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = value;
-	new_node->left = new_node->right = NULL;
 	if (!parent)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
 		return (NULL);
 	if (parent->left)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
-		new_node->parent = parent;
-		parent->left =  new_node;
 	}
-	else
-	{
-		parent->left = new_node;
-		new_node->parent = parent;
-	}
+	parent->left = new_node;
 	return (new_node);
 }

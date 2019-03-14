de5f256c2ff3f0cedba4e223d7132b4c444db08f
truong21 <543@holbertonschool.com>
2019-03-14 19:59:35 +0000

    binary tree height

---
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * max_h - computes the max depth of the two child nodes
+ * @a: depth of left node
+ * @b: depth of right node
+ * Return: max
+ */
+int max(int a, int b)
+{
+	if (a < b)
+		return (b);
+	else
+		return (a);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node fo the tree
+ * Return: height of tree value
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if ((!tree->left && !tree->right) && tree)
+		return (0);
+	return (1 + max(binary_tree_height(tree->left),
+			binary_tree_height(tree->right)));
+}

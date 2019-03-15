8fc4948c327666c6732be1d5f8578212e1f71b2f
truong21 <543@holbertonschool.com>
2019-03-15 23:36:11 +0000

    finds lowest common ancestor of two nodes

---
+++ 0x1C-binary_trees/100-binary_trees_ancestor.c
@@ -0,0 +1,51 @@
+#include "binary_trees.h"
+
+/**
+ * child_check - checks if node is a descendant of input node
+ * @ancestor: pointer to the ancestor node
+ * @child: pointer to child node
+ * Return: 1 if node are descendant of ancestor; 0 otherwise
+ */
+int child_check(const binary_tree_t *ancestor, const binary_tree_t *child)
+{
+	const binary_tree_t *tmp;
+
+	tmp = child;
+	while (tmp)
+	{
+		if (tmp == ancestor)
+			return (1);
+		tmp = tmp->parent;
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer to first node
+ * @second: pointer to second node
+ * Return: pointer to lowest common ancestor
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+					const binary_tree_t *second)
+{
+	const binary_tree_t *tmp;
+
+	if (!first || !second)
+		return (NULL);
+	tmp = first;
+	while (tmp)
+	{
+		if (child_check(tmp, second) == 1)
+			return ((binary_tree_t *)tmp);
+		tmp = tmp->parent;
+	}
+	tmp = second;
+	while (tmp)
+	{
+		if (child_check(tmp, first) == 1)
+			return ((binary_tree_t *)tmp);
+		tmp = tmp->parent;
+	}
+	return (NULL);
+}
